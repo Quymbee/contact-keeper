30b642da0b46247f58e03d31d03894b3cf8d397a
Quymbee Chen <quymbee@mattchen.com>
2020-05-28 11:04:16 -0400

    added user and auth functionality

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route       GET api/contacts
 // @descrip     Get all user contacts
 // @access      Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route       POST api/contacts
@@ -29,4 +43,4 @@ router.delete('/:id', (req, res) => {
   res.send('Delete contact');
 });
 
-module.exports = reouter;
+module.exports = router;

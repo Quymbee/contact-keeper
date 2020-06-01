44353da5179f02b3affa08ce1bd3babe7cf59432
Quymbee Chen <quymbee@mattchen.com>
2020-06-01 17:07:15 -0400

    fix heroku

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const path = require('path');
 
 const app = express();
 
-//Connect database
+// Connect Database
 connectDB();
 
-//init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //static folder
-  app.use(express.static('cient/build'));
+  // Set static folder
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
@@ -27,4 +27,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
